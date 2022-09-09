dda9f264bd1fb98ede7a808c8369bdf85933ecbc
aurelio <aurelio@aurelio-Lenovo-V15-G2-ALC>
2022-09-09 13:20:49 +0200

    trabalho uplando de imagem no service

--- src/main/java/com/dev/api/service/ProdutoImagensService.java
+++ src/main/java/com/dev/api/service/ProdutoImagensService.java
@@ -1,31 +1,63 @@
 package com.dev.api.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.dev.api.model.Produto;
 import com.dev.api.model.ProdutoImagens;
 import com.dev.api.repository.ProdutoImagensRepository;
+import com.dev.api.repository.ProdutoRepository;
 
 @Service
 public class ProdutoImagensService {
+	
+	private static String caminhoImagens = "/home/aurelio/Documents/imagensProjectSpring";
 
 	@Autowired
 	private ProdutoImagensRepository produtoImagem;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public List<ProdutoImagens> buscarTodos(){
 		return produtoImagem.findAll();
 	}
 	
-	public ProdutoImagens salvar(ProdutoImagens objecto) {
-		ProdutoImagens objectoNovo = produtoImagem.saveAndFlush(objecto);
+	public ProdutoImagens salvar(Long idProduto,MultipartFile arquivo) {
+		//pegar o id de produto
+		Produto produto = produtoRepository.findById(idProduto).get();
+		ProdutoImagens objecto = new ProdutoImagens();
 		
-		return objectoNovo; 			
+
+		try {
+			if (!arquivo.isEmpty()) {
+				byte[] bytes = arquivo.getBytes();
+				String nomeImagem = String.valueOf(produto.getId()) + arquivo.getOriginalFilename();
+				Path caminho = Paths
+						.get(caminhoImagens + String.valueOf(produto.getId()) + arquivo.getOriginalFilename());
+				Files.write(caminho, bytes);
+				objecto.setNome(nomeImagem);
+				
+				
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		objecto.setProduto(produto);
+		objecto = produtoImagem.saveAndFlush(objecto);
+		
+		return objecto; 			
 	}
 	
 	public ProdutoImagens alterar(ProdutoImagens objecto) {
-		
 		return produtoImagem.saveAndFlush(objecto);		
 	}
 	
